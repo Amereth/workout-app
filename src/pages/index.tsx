05e6bf901f2423531bca375a2f028f32d8aaafbe
Amereth <alek.melnyk@gmail.com>
2023-04-11 22:40:28 +0300

    sign in flow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
-import { type NextPage } from "next";
-import Head from "next/head";
+import { SignIn, SignInButton, useUser } from '@clerk/nextjs'
+import { type NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
+  const user = useUser()
+
+  if (!user.isSignedIn)
+    return (
+      <main>
+        <SignInButton>
+          <span className='button'>Bход</span>
+        </SignInButton>
+        <SignIn path='/sign-in' routing='path' signUpUrl='/sign-up' />
+      </main>
+    )
+
   return (
     <>
       <Head>
         <title>We Workout</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>home page</main>
+      <main className='grid h-screen place-content-center text-white'>
+        home page
+      </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
