e3bd44a0c62d1f92ce5e46cc58821b13db44b145
Amereth <alek.melnyk@gmail.com>
2023-04-11 22:20:21 +0300

    prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  pluginSearchDirs: false,
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
