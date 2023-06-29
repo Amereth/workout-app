895182dab32b1b663826317c6295fa239d1f3d42
Oleksandr Melnyk <alek.melnyk@gmail.com>
2023-06-30 00:00:37 +0300

    workout page

---
+++ src/pages/workout/[workoutId].tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
+import { buttonVariants } from '@/components/ui/button'
+import { PageHeader } from '@/src/components/PageHeader'
+import { api } from '@/src/utils/api'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { queryParamsToString } from '@/src/utils/queryParamsToString'
+import { AddExercise } from '@/src/components/workoutPage/AddExercise'
+
+export default function WorkoutPage() {
+  const {
+    query: { workoutId: workoutIdFromParams = '' },
+  } = useRouter()
+
+  const workoutId = queryParamsToString(workoutIdFromParams)
+
+  const { data: workout } = api.workouts.get.useQuery(workoutId)
+
+  if (!workout) return null
+
+  return (
+    <>
+      <PageHeader>
+        <Link href='/' className={buttonVariants({ variant: 'default' })}>
+          go home
+        </Link>
+      </PageHeader>
+
+      <main>
+        <AddExercise
+          workoutId={workout.id}
+          exercises={workout.workoutPlan.exercises}
+        />
+      </main>
+    </>
+  )
+}
