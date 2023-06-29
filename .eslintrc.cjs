a17c6b04a7c500d9cacbdd5571fc7ca03ec23de5
Oleksandr Melnyk <alek.melnyk@gmail.com>
2023-06-30 00:15:10 +0300

    eslint config (not working rules)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,7 @@ const config = {
     ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
   },
 }
 
