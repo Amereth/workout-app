dd8f2a9342d88ed25c31a9cffadf621f1f69af3d
Amereth <alek.melnyk@gmail.com>
2023-07-02 19:43:02 +0300

    installed react query devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import { type AppType } from 'next/app'
-
 import { api } from '~/utils/api'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import '@/styles/globals.css'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider>
+      <ReactQueryDevtools initialIsOpen />
+
       <Component {...pageProps} />
     </ClerkProvider>
   )
