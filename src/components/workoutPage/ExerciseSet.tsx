8015ef776679b38662c657e6f8ac0c2befde7b6b
Amereth <alek.melnyk@gmail.com>
2023-07-05 21:05:36 +0300

    list of sets design finished

--- src/components/workoutPage/ExerciseSet.tsx
+++ src/components/workoutPage/ExerciseSet.tsx
@@ -10,6 +10,8 @@ import {
 import { type Exercise, type Set } from '@prisma/client'
 import { NewSet, type NewSetProps } from './NewSet'
 import { TableCell } from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+import { X, Edit } from 'lucide-react'
 
 type SetControlsProps = {
   workoutId: NewSetProps['workoutId']
@@ -29,18 +31,34 @@ export function ExerciseSet({ workoutId, exercise, sets }: SetControlsProps) {
           <SheetTitle>{exercise.name}</SheetTitle>
         </SheetHeader>
 
-        {sets.map((set) => (
-          <div key={set.id} className='flex justify-between text-lg'>
-            <div className='flex flex-1 gap-x-4'>
-              <span>reps</span>
-              <span>{set.reps}</span>
-            </div>
-            <div className='flex flex-1 justify-end gap-x-4'>
-              <span>weight</span>
-              <span>{set.weight}</span>
-            </div>
-          </div>
-        ))}
+        <ul className='flex flex-col gap-y-4'>
+          {sets.map((set) => (
+            <li key={set.id}>
+              <div className='flex h-10 justify-between text-2xl'>
+                <div className='flex basis-1/2 justify-between'>
+                  <div>reps</div>
+                  <div>{set.reps}</div>
+                </div>
+
+                <div className='mx-4 w-[2px] self-stretch bg-black' />
+
+                <div className='flex basis-1/2 justify-between'>
+                  <div>{set.weight}</div>
+                  <div>weight</div>
+                </div>
+              </div>
+
+              <div className='mt-2 flex gap-x-4'>
+                <Button className='grow'>
+                  <Edit />
+                </Button>
+                <Button variant='destructive' className='grow'>
+                  <X />
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
 
         <NewSet exerciseId={exercise.id} workoutId={workoutId} />
       </SheetContent>
