1f764dbea4447c71f1d7483054611a1e52aff44d
Amereth <alek.melnyk@gmail.com>
2023-07-10 00:11:41 +0300

    added muscle group model

---
+++ prisma/migrations/20230709211058_created_muscle_group_model/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "MuscleGroup" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "MuscleGroup_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ExerciseToMuscleGroup" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MuscleGroup_name_key" ON "MuscleGroup"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ExerciseToMuscleGroup_AB_unique" ON "_ExerciseToMuscleGroup"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ExerciseToMuscleGroup_B_index" ON "_ExerciseToMuscleGroup"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToMuscleGroup" ADD CONSTRAINT "_ExerciseToMuscleGroup_A_fkey" FOREIGN KEY ("A") REFERENCES "Exercise"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToMuscleGroup" ADD CONSTRAINT "_ExerciseToMuscleGroup_B_fkey" FOREIGN KEY ("B") REFERENCES "MuscleGroup"("id") ON DELETE CASCADE ON UPDATE CASCADE;
